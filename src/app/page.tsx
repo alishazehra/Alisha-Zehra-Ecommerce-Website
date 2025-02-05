264b86357a6f4ee868e94d91c0e34691aa8e9d00
alisha zehra <alishazehra866@gmail.com>
2025-02-05 22:24:33 +0500

    improved efficiency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import ProductCards from './cart/page'
-
-const Home = () => {
+const Page = () => {
   return (
     <div>
-     
-      <ProductCards/>  
-      
+      <ProductCards/>
     </div>
   )
 }
 
-export default Home
+export default Page
+
+
+
+
+
+
+
